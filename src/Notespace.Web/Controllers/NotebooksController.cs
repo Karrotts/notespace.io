296f4554f14286ec45d5b9e7180115f77a72a643
wes <Wesleymiller1996@gmail.com>
2021-04-29 09:41:01 -0500

    Modified notebook page.

---
+++ src/Notespace.Web/Controllers/NotebooksController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Notespace.Web.Data;
+using Notespace.Web.Models;
+
+namespace Notespace.Web.Controllers
+{
+    public class NotebooksController : Controller
+    {
+        private readonly ApplicationIdentityContext _context;
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public NotebooksController(ApplicationIdentityContext context,
+               UserManager<ApplicationUser> userMgr,
+               SignInManager<ApplicationUser> signInMgr)
+        {
+            _userManager = userMgr;
+            _signInManager = signInMgr;
+            _context = context;
+        }
+
+        // GET: Notebooks
+        public async Task<IActionResult> Index()
+        {
+            var applicationIdentityContext = _context.Notebooks.Include(n => n.User);
+            return View(await applicationIdentityContext
+                                .Where(n => n.UserID == _userManager.GetUserId(User))
+                                .OrderByDescending(n => n.LastModified)
+                                .ToListAsync());
+        }
+
+        // GET: Notebooks/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notebook = await _context.Notebooks
+                .Include(n => n.User)
+                .FirstOrDefaultAsync(m => m.NotebookID == id);
+            if (notebook == null)
+            {
+                return NotFound();
+            }
+
+            return View(notebook);
+        }
+
+        // GET: Notebooks/Create
+        public IActionResult Create()
+        {
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Notebooks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NotebookID,UserID,Title,IsPublic,Color,LastModified")] Notebook notebook)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(notebook);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", notebook.UserID);
+            return View(notebook);
+        }
+
+        // GET: Notebooks/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notebook = await _context.Notebooks.FindAsync(id);
+            if (notebook == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", notebook.UserID);
+            return View(notebook);
+        }
+
+        // POST: Notebooks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("NotebookID,UserID,Title,IsPublic,Color,LastModified")] Notebook notebook)
+        {
+            if (id != notebook.NotebookID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(notebook);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NotebookExists(notebook.NotebookID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", notebook.UserID);
+            return View(notebook);
+        }
+
+        // GET: Notebooks/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notebook = await _context.Notebooks
+                .Include(n => n.User)
+                .FirstOrDefaultAsync(m => m.NotebookID == id);
+            if (notebook == null)
+            {
+                return NotFound();
+            }
+
+            return View(notebook);
+        }
+
+        // POST: Notebooks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var notebook = await _context.Notebooks.FindAsync(id);
+            _context.Notebooks.Remove(notebook);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NotebookExists(long id)
+        {
+            return _context.Notebooks.Any(e => e.NotebookID == id);
+        }
+    }
+}
